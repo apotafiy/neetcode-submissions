7968fff605689da976675ff3ce3f3913ab4304df
Artem Potafiy <61164823+apotafiy@users.noreply.github.com>
2026-06-12 09:39:02 -0700

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = [0, 1, 2]
+        for i in range(3, n + 1):
+            memo.append(memo[i - 1] + memo[i - 2])
+        print(memo)
+        return memo[n]
+        
